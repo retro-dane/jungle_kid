efd39caedc13599d1393652557a02c8cb52c3afb
retro-dane <43159871+retro-dane@users.noreply.github.com>
2024-02-26 19:52:39 -0500

    initial commit

---
+++ Assets/Scripts/Playermovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    public Character_Controller controller;
+
+    public float horizontalmove= 0f;
+    public float runspeed = 40f;
+    bool jump = false;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+       horizontalmove = Input.GetAxisRaw("Horizontal")*runspeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalmove*Time.fixedDeltaTime,false,jump);
+        jump = false;
+    }
+}
